e0f092c0396890c2cf42773f713510e8a575b556
Vaibhav Gupta <vaibhavpersonal1106@gmail.com>
2026-07-23 01:42:08 +0530

    initial spring boot setup - Day 1

---
+++ japanlearn/src/main/java/com/japanlearn/japanlearn/JapanlearnApplication.java
@@ -0,0 +1,13 @@
+package com.japanlearn.japanlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapanlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapanlearnApplication.class, args);
+	}
+
+}
